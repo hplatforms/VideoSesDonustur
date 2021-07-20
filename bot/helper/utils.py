aa66d56e9413220f42caae3f079182e4acd9fd05
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2021-07-20 21:57:06 +0300

    Update utils.py

--- bot/helper/utils.py
+++ bot/helper/utils.py
@@ -13,19 +13,19 @@ def on_task_complete():
 def add_task(message: Message):
     try:
       c_time = time.time()
-      msg = message.reply_text("```Video İşleme Alındı...```", quote=True)
+      msg = message.reply_text("`🟡 Video İşleme Alındı... 🟡\n\n⚙️ Motor: Pyrogram\n\n#indirme`", quote=True)
       filepath = message.download(
                 file_name=download_dir,
                 progress=progress_for_pyrogram,
                 progress_args=(
-                   "İndiriliyor...",
+                   "`İndiriliyor...`",
                     msg,
                     c_time
                 ))
-      msg.edit("```Video Kodlanıyor...```")
+      msg.edit("`🟣 Video Kodlanıyor... 🟣\n\n⚙️ Motor: FFMPEG\n\n#kodlama`")
       new_file = encode(filepath)
       if new_file:
-        msg.edit("```Video Kodlandı, Veriler Alınıyor...```")
+        msg.edit("`🟢 Video Kodlandı, Veriler Alınıyor... 🟢`")
         duration = get_duration(new_file)
         thumb = get_thumbnail(new_file, download_dir, duration / 4)
         width, height = get_width_height(new_file)
@@ -37,23 +37,23 @@ def add_task(message: Message):
         message.reply_video(
                 new_file,
                 caption=caption_str,
-                quote=True, 
-                supports_streaming=True, 
-                thumb=thumb, 
-                duration=duration, 
-                width=width, 
+                quote=True,
+                supports_streaming=True,
+                thumb=thumb,
+                duration=duration,
+                width=width,
                 height=height,
                 progress=progress_for_pyrogram,
                 progress_args=(
-                    f"{os.path.basename(new_file)} Yükleniyor...",
+                    f"`{os.path.basename(new_file)} Yükleniyor...`",
                     msg,
                     c_time
                 ))
         os.remove(new_file)
         os.remove(thumb)
-        msg.edit("```İşlem Bitti.```")
+        msg.edit("`🔴 İşlem Bitti. ✔️`")
       else:
-        msg.edit("```Dosyanızı kodlarken bir şeyler ters gitti.```")
+        msg.edit("`Dosyanızı kodlarken bir şeyler ters gitti.`")
         os.remove(filepath)
     except Exception as e:
       msg.edit(f"```{e}```")
