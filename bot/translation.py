f39d0fe1b203a6ecb1f754407220a8537dfba0e4
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2021-07-20 22:16:35 +0300

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,12 +1,12 @@
 class Translation(object):
        
-      DOWNLOAD_PROGRESS = "█"
-      UPLOAD_PROGRESS = "░"
+      DOWNLOAD_PROGRESS = "`█`"
+      UPLOAD_PROGRESS = "`░`"
       
-      PROGRESS = """
+      PROGRESS = """`
 Yüzde : {0}%
 Tamamlanan: {1}
 Toplam: {2}
 Hız: {3}/s
 Süre: {4}
-"""
+`"""
