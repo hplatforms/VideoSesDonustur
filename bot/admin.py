94993d721273b8251e8a8974ff6ba706735ab07b
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-04-09 22:37:03 +0300

    Update admin.py

--- bot/admin.py
+++ bot/admin.py
@@ -16,14 +16,14 @@ from pyrogram import Client
 
 @app.on_message(filters.command("restart") & filters.user(OWNER_ID))
 async def restart(_, m: Message):
-    restart_msg = await m.reply_text(text="`İntihar ediyom bekle...`")
-    await restart_msg.edit("`Ölmek üzereyim...`")
+    restart_message = await m.reply_text(text="`İntihar ediyom bekle...`")
+    await restart_message.edit("`Ölmek üzereyim...`")
     try:
         if HEROKU_API_KEY is not None:
             heroku_conn = heroku3.from_key(HEROKU_API_KEY)
             server = heroku_conn.app(HEROKU_APP_NAME)
             server.restart()
         else:
-            await restart_msg.edit("`Heroku Api Key ve uygulama adını ekleyin.`")
+            await restart_message.edit("`Heroku Api Key ve uygulama adını ekleyin.`")
     except Exception as e:
-        await restart_msg.edit(f"**İntihar bile edemedim:** `{e}`")
+        await restart_message.edit(f"**İntihar bile edemedim:** `{e}`")
