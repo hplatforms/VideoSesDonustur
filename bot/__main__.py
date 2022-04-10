ffe314917fb328b40031365166a3400abaee9102
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-04-10 04:41:44 +0300

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -18,6 +18,24 @@ from bot.helper.utils import add_task
 from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
 from .translation import Translation
 
+def ReadableTime(seconds: int) -> str:
+    result = ''
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+    if days != 0:
+        result += f'{days}d'
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+    if hours != 0:
+        result += f'{hours}h'
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+    if minutes != 0:
+        result += f'{minutes}m'
+    seconds = int(seconds)
+    result += f'{seconds}s'
+    return result
+
 video_mimetype = [
   "video/x-flv",
   "video/mp4",
