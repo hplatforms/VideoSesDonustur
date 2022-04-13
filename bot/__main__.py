56c5cfcb652d0620459479908484abf6890d42e8
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-04-13 17:28:15 +0300

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -116,11 +116,10 @@ async def stop(self, *args):
         texto = f"BUGÜN BENİM ÖLÜM GÜNÜM.\nYAŞADIĞIM SÜRE: `{ReadableTime(time.time() - botStartTime)}`"
                 await self.send_document(document='log.txt', caption=texto, chat_id=sudo_users)
             except Exception as t:
-                await self.send_message(text="`{t}`",
-                    chat_id=sudo_users)
-
-    await super().stop()
-    exit() 
+                LOGGER.warning(str(t))
+            await super().stop() 
+            LOGGER.info=(msg="app stopped.") 
+            exit() 
 
 app.run()
 
