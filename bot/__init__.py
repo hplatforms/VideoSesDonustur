5a65e0f0d59c8af58e2f80a6b30855ee6f7630ff
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-21 18:57:48 +0300

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -13,7 +13,6 @@ bot_token = os.environ.get("BOT_TOKEN")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
 sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
 send_logs_when_dying = str(os.environ.get("SEND_LOGS_WHEN_DYING", "True")).lower() == 'true'
-owner = int(os.environ.get("OWNER"))
 
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
